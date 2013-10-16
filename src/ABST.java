355736488c15719e87abc1bae4124a015aee0bab
Robbinsj-complab <robbins.j@husky.neu.edu>
2013-10-16 15:52:16 -0400

    work in comp lab

--- src/ABST.java
+++ src/ABST.java
@@ -1,4 +1,383 @@
+import tester.*;
 
-public interface ABST {
-
+/** represents a book **/
+class Book {
+    String title;
+    String author;
+    int price;
+    Book(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
 }
+
+/** represents a list of books **/
+interface ILoBook {
+    /** inserts book into list according to given filter **/
+    ILoBook insert(IBookComparator order, Book b);
+    /** returns whether the list is empty or not **/
+    boolean isEmpty();
+}
+
+/**represents an empty list of books **/
+class MtLoBook implements ILoBook {
+    MtLoBook() {
+        //empty constructor
+    }
+    /** inserts book into list according to given filter **/
+    public ILoBook insert(IBookComparator order, Book b) {
+        return new ConsLoBook(b, this);
+    }
+    /** returns whether the list is empty or not **/
+    public boolean isEmpty() {
+        return true;
+    }
+}
+
+/**represents populated list of books **/
+class ConsLoBook implements ILoBook {
+    Book first;
+    ILoBook  rest;
+    ConsLoBook(Book first, ILoBook rest) {
+        this.first = first;
+        this.rest = rest;
+    }
+    /** inserts book into list according to given filter **/
+    public ILoBook insert(IBookComparator order, Book b) {
+        if (order.compare(this.first, b) > 0) {
+            return new ConsLoBook(this.first, this.rest.insert(order, b));
+        }
+        else {
+            return new ConsLoBook(b, this);
+        }
+    }
+    /** returns whether the list is empty or not **/
+    public boolean isEmpty() {
+        return false;
+    }
+}
+/** interface for filter classes 
+ * to compare books
+ **/
+interface IBookComparator {
+    int compare(Book b1, Book b2);
+}
+
+/** filter class for comparing books by title **/
+class BooksByTitle implements IBookComparator {
+    /**
+     * returns 
+     * <0 if b1<b2
+     * 0 if b1=b2
+     * >0 if b1>b2
+     */
+    public int compare(Book b1, Book b2) {
+        return b1.title.compareTo(b2.title);
+    }
+}
+
+/** filter class for comparing books by Author **/
+class BooksByAuthor implements IBookComparator {
+    /**
+     * returns 
+     * <0 if b1<b2
+     * 0 if b1=b2
+     * >0 if b1>b2
+     */
+    public int compare(Book b1, Book b2) {
+        return b1.author.compareTo(b2.author);
+    }
+}
+
+/** filter class for comparing books by price **/
+class BooksByPrice implements IBookComparator {
+    /**
+     * returns 
+     * <0 if b1<b2
+     * 0 if b1=b2
+     * >0 if b1>b2
+     */
+    public int compare(Book b1, Book b2) {
+        return b1.price - b2.price;
+    }
+}
+
+/** abstract class of nodes and leafs
+ * for binary search tree **/
+abstract class ABST {
+    IBookComparator order;
+    ABST(IBookComparator order) {
+        this.order = order;
+    }
+    /** creates new tree with b in correct place
+     * decided by filter
+     */
+    abstract ABST insert(Book b);
+    /** produces first book in binary tree **/
+    abstract Book getFirst();
+    /** produces ABST search tree with first book removed **/
+    abstract ABST getRest();
+    /** returns whether node is empty **/
+    boolean isEmpty() {
+        return false;
+    }
+    /**for getRest()
+     *  finds highest value in branch and adds ABST to right pointer **/
+    abstract ABST appendToEnd(ABST b);
+    /**for getRest()
+     *  finds lowest value in branch and adds ABST to left pointer **/
+    abstract ABST appendToStart(ABST b);
+    /** determines whether given tree is identical to this tree **/
+    abstract boolean sameTree(ABST that);
+    /**helper method
+     *  returns data held by Node **/
+    abstract Book getData();
+    /**helper method
+     *  returns left pointer of Node **/
+    abstract ABST getLeft();
+    /**helper method
+     *  returns right pointer of Node **/
+    abstract ABST getRight();
+    /** returns whether this tree contains the same books as the given tree **/
+    abstract boolean sameData(ABST that);
+    /** returns whether this tree has Book b **/
+    abstract boolean inTree(Book b);
+    /** combines given sorted list and binary tree into sorted list **/
+    abstract ILoBook buildList(ILoBook that);
+    /** returns whether given list is the same as this binary tree **/
+    abstract boolean sameAsList(ILoBook that);
+}
+
+/** represents empty node in binary search tree
+ * used for ending a branch **/
+class Leaf extends ABST {
+    Leaf(IBookComparator order) {
+        super(order);
+    }
+    /** returns Node with b and pointers to 2 leaf **/
+    ABST insert(Book b) {
+        return new Node(this.order, b, this, this);
+    }
+    /** produces first book in binary tree **/
+    Book getFirst() {
+        throw new RuntimeException("No first in an empty tree");
+    }
+    /** produces ABST search tree with first book removed **/
+    ABST getRest() {
+        throw new RuntimeException("No rest of an empty tree");
+    }
+    /** returns whether node is empty **/
+    boolean isEmpty() {
+        return true;
+    }
+    /**for getRest()
+     *  finds highest value in branch and adds ABST to right pointer **/
+    ABST appendToEnd(ABST b) {
+        throw new RuntimeException("no tree to append to");
+    }
+    /**for getRest()
+     *  finds lowest value in branch and adds ABST to left pointer **/
+    ABST appendToStart(ABST b) {
+        throw new RuntimeException("no tree to append to");
+    }
+    /** determines whether given tree is identical to this tree **/
+    boolean sameTree(ABST that) {
+        return that.isEmpty();
+    }
+    /** returns whether this tree contains the same books as the given tree **/
+    boolean sameData(ABST that) {
+        return that.isEmpty();
+    }
+    /**helper method
+     *  returns data held by Node **/
+    Book getData() {
+        throw new RuntimeException("no data in empty tree");
+    }
+    /**helper method
+     *  returns left pointer of Node **/
+    ABST getLeft() {
+        throw new RuntimeException("no pointer in empty tree");
+    }
+    /**helper method
+     *  returns right pointer of Node **/
+    ABST getRight() {
+        throw new RuntimeException("no pointer in empty tree");
+    }
+    /** returns whether this tree has Book b **/
+    boolean inTree(Book b) {
+        return false;
+    }
+    /** combines given sorted list and binary tree into sorted list **/
+    ILoBook buildList(ILoBook that) {
+        return that;
+    }
+    /** returns whether given list is the same as this binary tree **/
+    boolean sameAsList(ILoBook that) {
+        return that.isEmpty();
+    }
+}
+
+/** represents node containing information
+ *  in binary search tree **/
+class Node extends ABST {
+    Book data;
+    ABST left;
+    ABST right;
+    Node(IBookComparator order, Book data, ABST left, ABST right) {
+        super(order);
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+    
+    /**helper method
+     *  returns data held by Node **/
+    public Book getData() {
+        return this.data;
+    }
+    /**helper method
+     *  returns left pointer of Node **/
+    public ABST getLeft() {
+        return this.left;
+    }
+    /**helper method
+     *  returns right pointer of Node **/
+    public ABST getRight() {
+        return this.right;
+    }
+    
+    /** inserts b into correct order using filter 
+     * when values are identical they are added to the right
+     **/
+    public ABST insert(Book b) {
+        if (this.order.compare(this.data, b) == 0) {
+            return this.right = new Node(this.order, b, new Leaf(this.order), this.right);
+        }
+        else {
+            if (this.order.compare(this.data, b) > 0) {
+                return this.right.insert(b);
+            }
+            else {
+                return this.left.insert(b);
+            }
+        }
+    }
+    
+    /** produces first book in binary tree **/
+    public Book getFirst() {
+        return this.data;
+    }
+    
+    /** produces ABST search tree with first book removed **/
+    public ABST getRest() {
+        if (!this.left.isEmpty()) {
+            this.left.appendToEnd(this.right);
+            return this.left;
+        }
+        else {
+            this.right.appendToStart(this.left);
+            return this.right;
+        }
+    }
+    /**Helper for getRest()
+     *  finds highest value in branch and adds ABST to right pointer **/
+    public ABST appendToEnd(ABST b) {
+        if (this.right.isEmpty()) {
+            this.right = b;
+            return this;
+        }
+        else {
+            return this.right.appendToEnd(b);
+        }
+    }
+    /**Helper for getRest()
+     * finds lowest value in branch and adds ABST to left pointer **/
+    public ABST appendToStart(ABST b) {
+        if (this.left.isEmpty()) {
+            this.left = b;
+            return this;
+        }
+        else {
+            return this.left.appendToStart(b);
+        }
+    }
+    
+    /** determines whether given tree is identical to this tree **/
+    public boolean sameTree(ABST that) {
+        if (that.isEmpty()) {
+            return false;
+        }
+        else {
+            if (this.data == that.getData()) {
+                return true &&
+                        this.left.sameTree(getLeft()) && 
+                        this.right.sameTree(getRight());
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    /** returns whether given tree has the same data as this tree **/
+    public boolean sameData(ABST that) {
+        if (this.sameTree(that)){
+            return true;
+        }
+        else {
+            if (that.inTree(this.data)) {
+                return true && this.getRest().sameData(that);
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    /** returns whether this tree has Book b **/
+    boolean inTree(Book b) {
+        if (this.data.equals(b)) {
+            return true;
+        }
+        else {
+            return this.left.inTree(b) || this.right.inTree(b);
+        }
+    }
+    
+    /** combines given sorted list and binary tree into sorted list **/
+    ILoBook buildList(ILoBook that) {
+        return this.getRest().buildList(that.insert(this.order, this.data));
+    }
+    /** returns whether given list is the same as this binary tree **/
+    boolean sameAsList(ILoBook that) {
+        return that.equals(this.buildList(new MtLoBook()));
+    }
+    
+}
+
+class ExamplesBinaryTree {
+    Book tale = new Book("the tale of two cities", "Charles Dickons", 10);
+    Book lord = new Book("LOTR", "J.R.R. Tolkien", 15);
+    Book jeff = new Book("My Life", "Jeff Robbins", 11);
+    Book tao = new Book("Tao te Ching", "Lao tzu", 8);
+    
+    IBookComparator authorComp = new BooksByAuthor();
+    IBookComparator titleComp = new BooksByTitle();
+    IBookComparator priceComp = new BooksByPrice();
+    
+    ABST leafPrice = new Leaf(this.priceComp);
+    ABST leafTitle = new Leaf(this.titleComp);
+    ABST leafAuthor = new Leaf(this.authorComp);
+    
+    ABST priceTree = new Node(this.priceComp, this.tale, 
+            new Node(this.priceComp, this.tao, this.leafPrice, this.leafPrice),
+            new Node(this.priceComp, this.lord,
+                    new Node(this.priceComp, this.jeff, this.leafPrice, this.leafPrice),
+                    this.leafPrice));
+    ABST titleTree = new Node(this.titleComp, this.jeff,
+            new Node(this.titleComp, this.lord, this.leafTitle, this.leafTitle),
+            new Node(this.titleComp, this.tale,
+                    new Node(this.titleComp, this.tao, this.leafTitle, this.leafTitle), 
+                    this.leafTitle));
+    }
+
+
--- src/Strings.java
+++ src/Strings.java
@@ -9,18 +9,54 @@
  */
 import tester.*;
 
+// to compare two string
+interface StringsCompare {
+	public boolean comesBefore(String s1, String s2);
+}
+
+//compare two string lexicographically
+
+class StringLexComp implements StringsCompare {
+	public boolean comesBefore(String s1, String s2) {
+		if ( s1.compareTo(s2) < 0 )
+		     { return true; }
+		else 
+		     { return false; }
+	}
+}
+
+//compare if the longer string comes first
+
+class StringLengthComp implements StringsCompare {
+    public boolean comesBefore(String s1, String s2) {
+        if ( s1.length() > s2.length() ) { 
+            return true; 
+        }
+        else {
+            return false;
+        }
+    }
+}
+
 // to represent a list of Strings
 interface ILoS {
-// combine all Strings in this list into one
-    String combine();
-//returns content of list
-    String contents();
+
 //determines whether given list is sorted
-    boolean isSorted();
-// insert a string to the list in alphabetic order 
-    ILoS insert(String that);
-//merge given list into this sorted list 
-    ILoS merge(ILoS that);
+// by a given request
+boolean isSorted(StringsCompare compare);
+
+
+//Helper for merge:
+//insert a string to the list in required order 
+ILoS insert(String that, StringsCompare compare);
+
+//merge given sorted list into this sorted list 
+//in required order
+ILoS merge(ILoS that, StringsCompare compare);
+
+//sort the list as required 
+ILoS sort(StringsCompare compare);
+
 }
 
 // to represent an empty list of Strings
@@ -29,36 +65,38 @@ class MtLoS implements ILoS {
         //empty
     }
   
-// combine all Strings in this list into one
-    public String combine() {
-        return "";
-    }  
+
 //returns true because empty list is alphabetical
-    public boolean isSorted() {
+    public boolean isSorted(StringsCompare compare) {
         return true;
     }
-//returns empty string because Mt list is empty
-    public String contents() {
-        return "";
-    }
- // insert a string to the list in alphabetic order 
-    public ILoS insert(String that) {
-        return new ConsLoS(that, this);
-    }
-//merge two list into one sorted list
-    public ILoS merge(ILoS that) {
-        return that;
-    }
+  
+    
+// insert a string to the list in alphabetic order 
+    public ILoS insert(String that, StringsCompare compare) {
+        return new OneLoS(that, this);
+    }   
+//merge given sorted list into this sorted list 
+//in required order
+  public ILoS merge(ILoS that, StringsCompare compare) {
+      return that;
+  }
+
+//sort the list as required 
+public ILoS sort(StringsCompare compare) {
+    return this;
+}
 }
 
-// to represent a nonempty list of Strings
-class ConsLoS implements ILoS {
+
+
+
+
+// to represent a non-empty list
+abstract class ConsLoS implements ILoS {
     String first;
-    ILoS rest;
-  
-    ConsLoS(String first, ILoS rest) {
-        this.first = first;
-        this.rest = rest;  
+    ConsLoS(String first) {
+        this.first = first; 
     }
 
  /*
@@ -77,87 +115,167 @@ class ConsLoS implements ILoS {
   
   */
   
-// combine all Strings in this list into one
-    public String combine() {
-        return this.first.concat(this.rest.combine());
-    }  
-//returns if list is sorted alphabetically 
-    public boolean isSorted() {
-        if (this.rest.contents() == "") {
-            return true;
+
+//returns if list is sorted as required 
+    public abstract boolean isSorted(StringsCompare compare);
+    
+
+
+//insert a string to the list in required order     
+    public abstract ConsLoS insert(String that, StringsCompare compare);
+
+//merge two ordered list into given order    
+    public abstract ILoS merge(ILoS that, StringsCompare compare); 
+
+//sort the list as required 
+    public abstract ILoS sort(StringsCompare compare);
+}
+
+
+
+
+
+//to represent a list with only 1 string 
+class OneLoS extends ConsLoS {
+    MtLoS rest;
+   
+    OneLoS(String first,MtLoS rest) {
+        super(first);
+        this.rest = rest;
+    }
+    
+//returns true because there is only one string
+    public boolean isSorted(StringsCompare compare) {
+        return true;
+    }
+
+//insert a string to the list in required order   
+    public ConsLoS insert(String that, StringsCompare compare) {
+        if (compare.comesBefore(that, this.first)) {
+            return new MultiLoS(that, this); 
         }
         else {
-            if (this.first.compareTo(this.rest.contents()) > 0) {
-                return false;
-            }
-            else {
-                return this.rest.isSorted();
-            }
+            return new MultiLoS(this.first, new OneLoS(that, new MtLoS()));       	
         }
     }
-//returns the contents of first
-    public String contents() {
-        return this.first;
+//merge two ordered list into given order    
+    public ILoS merge(ILoS that, StringsCompare compare) {
+        return that.insert(this.first, compare);
     }
-// insert a string to the list in alphabetic order     
-    public ILoS insert(String that) {
-        if (this.first.compareTo(that) <= 0) {
-            return new ConsLoS(this.first, this.rest.insert(that));
-        }
-        else {
-            return new ConsLoS(that, this); 
-        }
-    }
-//merge two list into one sorted list
-    public ILoS merge(ILoS that) {
-        return this.rest.merge(that.insert(this.first));
+//sort the list as required 
+    public ILoS sort(StringsCompare compare) {
+        return this;
     }
 }
 
-// to represent examples for lists of strings
-class ExamplesStrings {
-    ExamplesStrings() {
-        //empty
+//to represent a list with more than one strings 
+class MultiLoS extends ConsLoS {
+    ConsLoS rest;
+ 
+    MultiLoS(String first,ConsLoS rest) {
+        super(first);
+        this.rest = rest;
+    }
+
+// check if the list is sorted as required 
+    public boolean isSorted(StringsCompare compare) {
+        return compare.comesBefore(this.first, this.rest.first) &&
+                this.rest.isSorted(compare);
+    }
+//insert a string to the list in required order   
+    public ConsLoS insert(String that, StringsCompare compare) {
+        if (compare.comesBefore(that, this.first)) {
+            return new MultiLoS(that, this); 
+        }
+        else {
+            return new MultiLoS(this.first, this.rest.insert(that, compare));    	
+        }
     }
   
-    ILoS mary = new ConsLoS("Mary ",
-                    new ConsLoS("had ",
-                            new ConsLoS("a ",
-                                    new ConsLoS("little ",
-                                        new ConsLoS("lamb.", new MtLoS())))));
-    ILoS alphabet = new ConsLoS("a",
-            new ConsLoS("b", 
-                    new ConsLoS("d", new MtLoS())));
-    ILoS alphabet1 = new ConsLoS("a",
-            new ConsLoS("b", 
-                    new ConsLoS("c", new MtLoS())));
-    ILoS alphabetCom = new ConsLoS("a",
-            new ConsLoS("a",
-                    new ConsLoS("b", 
-                            new ConsLoS("b", 
-                                    new ConsLoS("c", 
-                                           new ConsLoS("d",
-                                                   new MtLoS()
-                        ))))));
-    boolean testCombine(Tester t) {
-        return 
-                t.checkExpect(this.mary.combine(), "Mary had a little lamb.");
+//merge two ordered list into given order    
+    public ILoS merge(ILoS that, StringsCompare compare) {
+        return this.rest.merge(that.insert(this.first, compare), compare);
     }
     
+//sort the list as required 
+    public ILoS sort(StringsCompare compare) {
+        return this.rest.sort(compare).insert(this.first, compare);
+    }
+}
+
+
+
+// to represent examples for lists of strings
+class ExamplesStrings {
+  
+    ILoS mary = new MultiLoS("Mary ",
+                    new MultiLoS("had ",
+                            new MultiLoS("a ",
+                                    new MultiLoS("little ",
+                                        new OneLoS("lamb.", new MtLoS())))));
+    ILoS alphabet = new MultiLoS("aaa",
+            new MultiLoS("bb", 
+                    new OneLoS("dd", new MtLoS())));
+    
+    ILoS alphabet1 = new MultiLoS("aaa",
+            new MultiLoS("bb", 
+                    new OneLoS("c", new MtLoS())));
+    ILoS alphabetCom = new MultiLoS("aaa",
+            new MultiLoS("aaa",
+                    new MultiLoS("bb", 
+                            new MultiLoS("bb", 
+                                    new MultiLoS("c", 
+                                           new OneLoS("dd",
+                                                   new MtLoS()
+                        ))))));
+    ILoS lengthCom = new MultiLoS("aaa",
+            new MultiLoS("aaa",
+                    new MultiLoS("bb", 
+                            new MultiLoS("bb", 
+                                    new MultiLoS("dd", 
+                                           new OneLoS("c",
+                                                   new MtLoS()
+                        ))))));
     boolean testIsSorted(Tester t) {
         return
-                t.checkExpect(this.mary.isSorted(), false) &&
-                t.checkExpect(this.alphabet.isSorted(), true);
+                t.checkExpect(this.mary.isSorted(new StringLexComp()), false) &&
+                t.checkExpect(this.alphabet.isSorted(new StringLexComp()), true) &&
+                t.checkExpect(this.alphabet1.isSorted(new StringLengthComp()), true) &&
+                t.checkExpect(this.alphabetCom.isSorted(new StringLengthComp()), false);
+                
     }
     boolean testInsert(Tester t) {
         return
-                t.checkExpect(this.alphabet.insert("c"), new ConsLoS("a",
-                        new ConsLoS("b", new ConsLoS("c", 
-                                new ConsLoS("d", new MtLoS())))));
+                t.checkExpect(this.alphabet.insert("c", new StringLexComp()), new MultiLoS("aaa",
+                        new MultiLoS("bb", new MultiLoS("c", 
+                                new OneLoS("dd", new MtLoS()))))) &&
+                t.checkExpect(this.alphabet.insert("c", new StringLengthComp()), new MultiLoS("aaa",
+                        new MultiLoS("bb", new MultiLoS("dd", 
+                                new OneLoS("c", new MtLoS())))));
+                                
     }
     boolean testMerge(Tester t) {
         return
-                t.checkExpect(this.alphabet.merge(this.alphabet1),
-                        this.alphabetCom );
+                t.checkExpect(this.alphabet.merge(this.alphabet1,new StringLexComp() ),
+                        this.alphabetCom ) &&
+                t.checkExpect(this.alphabet.merge(this.alphabet1,new StringLengthComp() ),
+                        this.lengthCom );        
+    }
+    boolean testSort(Tester t) {
+    	return
+    			t.checkExpect(this.mary.sort(new StringLexComp()), 
+    					new MultiLoS("Mary ",
+    		                    new MultiLoS("a ",
+    		                            new MultiLoS("had ",
+    		                                    new MultiLoS("lamb.",
+    		                                        new OneLoS("little ", new MtLoS())))))) &&
+    	t.checkExpect(this.mary.sort(new StringLengthComp()), 
+				new MultiLoS("little ",
+	                    new MultiLoS("lamb.",
+	                            new MultiLoS("Mary ",
+	                                    new MultiLoS("had ",
+	                                        new OneLoS("a ", new MtLoS()))))));
+    		                                        
     }
 }
+ 
