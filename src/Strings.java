47fe606d2239d9d7a7665de73a2b2ef22ce5d570
Robbinsj-complab <robbins.j@husky.neu.edu>
2013-10-17 15:23:13 -0400

    fixed strings

--- src/Strings.java
+++ src/Strings.java
@@ -41,21 +41,30 @@ class StringLengthComp implements StringsCompare {
 // to represent a list of Strings
 interface ILoS {
 
-//determines whether given list is sorted
-// by a given request
-boolean isSorted(StringsCompare compare);
+    //determines whether given list is sorted
+    // by a given request
+    boolean isSorted(StringsCompare compare);
 
 
-//Helper for merge:
-//insert a string to the list in required order 
-ILoS insert(String that, StringsCompare compare);
+    //Helper for merge:
+    //insert a string to the list in required order 
+    ILoS insert(String that, StringsCompare compare);
 
-//merge given sorted list into this sorted list 
-//in required order
-ILoS merge(ILoS that, StringsCompare compare);
+    //merge given sorted list into this sorted list 
+    //in required order
+    ILoS merge(ILoS that, StringsCompare compare);
 
-//sort the list as required 
-ILoS sort(StringsCompare compare);
+    //sort the list as required 
+    ILoS sort(StringsCompare compare);
+
+    //returns if list is empty
+    boolean isEmpty();
+    
+    //returns contents of list
+    String contents();
+    
+    //combines lists
+    ILoS combine(ILoS that);
 
 }
 
@@ -64,8 +73,15 @@ class MtLoS implements ILoS {
     MtLoS() {
         //empty
     }
+    //returns empty string
+    public String contents() {
+        return "";
+    }
   
-
+    //returns contents of ILoS
+    public boolean isEmpty() {
+        return true;
+    }
 //returns true because empty list is alphabetical
     public boolean isSorted(StringsCompare compare) {
         return true;
@@ -74,18 +90,22 @@ class MtLoS implements ILoS {
     
 // insert a string to the list in alphabetic order 
     public ILoS insert(String that, StringsCompare compare) {
-        return new OneLoS(that, this);
+        return new ConsLoS(that, this);
     }   
-//merge given sorted list into this sorted list 
-//in required order
-  public ILoS merge(ILoS that, StringsCompare compare) {
-      return that;
-  }
+    //merge given sorted list into this sorted list 
+    //in required order
+    public ILoS merge(ILoS that, StringsCompare compare) {
+        return that;
+    }
 
-//sort the list as required 
-public ILoS sort(StringsCompare compare) {
-    return this;
-}
+  //sort the list as required 
+    public ILoS sort(StringsCompare compare) {
+        return this;
+    }
+  //combines lists
+    public ILoS combine(ILoS that) {
+        return that;
+    }
 }
 
 
@@ -93,10 +113,16 @@ public ILoS sort(StringsCompare compare) {
 
 
 // to represent a non-empty list
-abstract class ConsLoS implements ILoS {
+class ConsLoS implements ILoS {
     String first;
-    ConsLoS(String first) {
+    ILoS rest;
+    ConsLoS(String first, ILoS rest) {
         this.first = first; 
+        this.rest = rest;
+    }
+  //returns empty string
+    public String contents() {
+        return this.first;
     }
 
  /*
@@ -114,164 +140,126 @@ abstract class ConsLoS implements ILoS {
   ... this.rest.combine() ...             -- String
   
   */
-  
-
+  //returns contents of ILoS
+    public boolean isEmpty() {
+        return false;
+    }
 //returns if list is sorted as required 
-    public abstract boolean isSorted(StringsCompare compare);
-    
-
-
-//insert a string to the list in required order     
-    public abstract ConsLoS insert(String that, StringsCompare compare);
-
-//merge two ordered list into given order    
-    public abstract ILoS merge(ILoS that, StringsCompare compare); 
-
-//sort the list as required 
-    public abstract ILoS sort(StringsCompare compare);
-}
-
-//to represent a list with only 1 string 
-class OneLoS extends ConsLoS {
-    MtLoS rest;
-   
-    OneLoS(String first,MtLoS rest) {
-        super(first);
-        this.rest = rest;
-    }
-    
-//returns true because there is only one string
     public boolean isSorted(StringsCompare compare) {
-        return true;
-    }
-
-//insert a string to the list in required order   
-    public ConsLoS insert(String that, StringsCompare compare) {
-        if (compare.comesBefore(that, this.first)) {
-            return new MultiLoS(that, this); 
+        if (this.rest.isEmpty()) {
+            return true;
         }
         else {
-            return new MultiLoS(this.first, new OneLoS(that, new MtLoS()));       	
+            return compare.comesBefore(this.first, this.rest.contents()) &&
+                    this.rest.isSorted(compare);
         }
-    }
-//merge two ordered list into given order    
-    public ILoS merge(ILoS that, StringsCompare compare) {
-        return that.insert(this.first, compare);
-    }
-//sort the list as required 
-    public ILoS sort(StringsCompare compare) {
-        return this;
-    }
-}
-
-//to represent a list with more than one strings 
-class MultiLoS extends ConsLoS {
-    ConsLoS rest;
- 
-    MultiLoS(String first,ConsLoS rest) {
-        super(first);
-        this.rest = rest;
-    }
-
-// check if the list is sorted as required 
-    public boolean isSorted(StringsCompare compare) {
-        return compare.comesBefore(this.first, this.rest.first) &&
-                this.rest.isSorted(compare);
-    }
-//insert a string to the list in required order   
-    public ConsLoS insert(String that, StringsCompare compare) {
-        if (compare.comesBefore(that, this.first)) {
-            return new MultiLoS(that, this); 
-        }
-        else {
-            return new MultiLoS(this.first, this.rest.insert(that, compare));    	
-        }
-    }
-  
-//merge two ordered list into given order    
-    public ILoS merge(ILoS that, StringsCompare compare) {
-        return this.rest.merge(that.insert(this.first, compare), compare);
     }
     
-//sort the list as required 
+
+
+  //insert a string to the list in required order   
+    public ILoS insert(String that, StringsCompare compare) {
+        if (compare.comesBefore(that, this.first)) {
+            return new ConsLoS(that, this); 
+        }
+        else {
+            return new ConsLoS(this.first, this.rest.insert(that, compare));       
+        }
+    }
+
+  //merge two ordered list into given order    
+    public ILoS merge(ILoS that, StringsCompare compare) {
+        return this.combine(that).sort(compare);
+    }
+
+  //sort the list as required 
     public ILoS sort(StringsCompare compare) {
         return this.rest.sort(compare).insert(this.first, compare);
     }
+  //combines lists
+    public ILoS combine(ILoS that) {
+        return this.rest.combine(new ConsLoS(this.first, that));
+    }
 }
-
 
 
 // to represent examples for lists of strings
 class ExamplesStrings {
   
-    ILoS mary = new MultiLoS("Mary ",
-                    new MultiLoS("had ",
-                            new MultiLoS("a ",
-                                    new MultiLoS("little ",
-                                        new OneLoS("lamb.", new MtLoS())))));
-    ILoS alphabet = new MultiLoS("aaa",
-            new MultiLoS("bb", 
-                    new OneLoS("dd", new MtLoS())));
+    ILoS mary = new ConsLoS("Mary ",
+                    new ConsLoS("had ",
+                            new ConsLoS("a ",
+                                    new ConsLoS("little ",
+                                        new ConsLoS("lamb. ",
+                                                new MtLoS())))));
+    ILoS alphabet = new ConsLoS("aaa",
+            new ConsLoS("bb", 
+                    new ConsLoS("dddd", new MtLoS())));
     
-    ILoS alphabet1 = new MultiLoS("aaa",
-            new MultiLoS("bb", 
-                    new OneLoS("c", new MtLoS())));
-    ILoS alphabetCom = new MultiLoS("aaa",
-            new MultiLoS("aaa",
-                    new MultiLoS("bb", 
-                            new MultiLoS("bb", 
-                                    new MultiLoS("c", 
-                                           new OneLoS("dd",
+    ILoS alphabet1 = new ConsLoS("aaa",
+            new ConsLoS("bb", 
+                    new ConsLoS("c", new MtLoS())));
+    ILoS alphabetCom = new ConsLoS("aaa",
+            new ConsLoS("aaa",
+                    new ConsLoS("bb", 
+                            new ConsLoS("bb", 
+                                    new ConsLoS("c", 
+                                           new ConsLoS("dddd",
                                                    new MtLoS()
                         ))))));
-    ILoS lengthCom = new MultiLoS("aaa",
-            new MultiLoS("aaa",
-                    new MultiLoS("bb", 
-                            new MultiLoS("bb", 
-                                    new MultiLoS("dd", 
-                                           new OneLoS("c",
+    ILoS lengthCom = new ConsLoS("c",
+            new ConsLoS("bb",
+                    new ConsLoS("bb", 
+                            new ConsLoS("aaa", 
+                                    new ConsLoS("aaa", 
+                                           new ConsLoS("dddd",
                                                    new MtLoS()
                         ))))));
     boolean testIsSorted(Tester t) {
         return
-                t.checkExpect(this.mary.isSorted(new StringLexComp()), false) &&
-                t.checkExpect(this.alphabet.isSorted(new StringLexComp()), true) &&
-                t.checkExpect(this.alphabet1.isSorted(new StringLengthComp()), true) &&
-                t.checkExpect(this.alphabetCom.isSorted(new StringLengthComp()), false);
+                t.checkExpect(this.mary.isSorted(
+                        new StringLexComp()), false) &&
+                t.checkExpect(this.alphabet.isSorted(
+                        new StringLexComp()), true) &&
+                t.checkExpect(this.alphabet1.isSorted(
+                        new StringLengthComp()),false) &&
+                t.checkExpect(this.alphabetCom.isSorted(
+                        new StringLengthComp()), false);
                 
     }
     boolean testInsert(Tester t) {
         return
-                t.checkExpect(this.alphabet.insert("c", new StringLexComp()), new MultiLoS("aaa",
-                        new MultiLoS("bb", new MultiLoS("c", 
-                                new OneLoS("dd", new MtLoS()))))) &&
-                t.checkExpect(this.alphabet.insert("c", new StringLengthComp()), new MultiLoS("aaa",
-                        new MultiLoS("bb", new MultiLoS("dd", 
-                                new OneLoS("c", new MtLoS())))));
+                t.checkExpect(this.alphabet.insert("c", new StringLexComp()),
+                        new ConsLoS("aaa",
+                        new ConsLoS("bb", new ConsLoS("c", 
+                                new ConsLoS("dddd", new MtLoS())))));
                                 
     }
     boolean testMerge(Tester t) {
         return
-                t.checkExpect(this.alphabet.merge(this.alphabet1,new StringLexComp() ),
+                t.checkExpect(this.alphabet.merge(this.alphabet1,
+                        new StringLexComp() ),
                         this.alphabetCom ) &&
-                t.checkExpect(this.alphabet.merge(this.alphabet1,new StringLengthComp() ),
+                t.checkExpect(this.alphabet.merge(this.alphabet1,
+                        new StringLengthComp() ),
                         this.lengthCom );        
     }
     boolean testSort(Tester t) {
-    	return
-    			t.checkExpect(this.mary.sort(new StringLexComp()), 
-    					new MultiLoS("Mary ",
-    		                    new MultiLoS("a ",
-    		                            new MultiLoS("had ",
-    		                                    new MultiLoS("lamb.",
-    		                                        new OneLoS("little ", new MtLoS())))))) &&
-    	t.checkExpect(this.mary.sort(new StringLengthComp()), 
-				new MultiLoS("little ",
-	                    new MultiLoS("lamb.",
-	                            new MultiLoS("Mary ",
-	                                    new MultiLoS("had ",
-	                                        new OneLoS("a ", new MtLoS()))))));
-    		                                        
+        return
+                t.checkExpect(this.mary.sort(new StringLexComp()), 
+                        new ConsLoS("Mary ",
+                                new ConsLoS("a ",
+                                        new ConsLoS("had ",
+                                                new ConsLoS("lamb. ",
+                                                        new ConsLoS("little ",
+                                                                new MtLoS())))))) &&
+                t.checkExpect(this.mary.sort(new StringLengthComp()), 
+                        new ConsLoS("a ",
+                        new ConsLoS("had ",
+                                new ConsLoS("Mary ",
+                                        new ConsLoS("lamb. ",
+                                            new ConsLoS("little ",
+                                                    new MtLoS()))))));
     }
 }
  
